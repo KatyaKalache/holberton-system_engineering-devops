0f90aca802af7c836af8443df8b764ddb2f2ff9d
KatyaKalache <katya@kalache.fr>
2017-09-09 05:08:29 +0000

    rm file check part

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,4 @@
 # editing class-wp-locale.phpp name to .php
-file { '/var/www/html/wp-setting.php':
- ensure => file,
-} ->
 exec { '/var/www/html/wp-setting.php':
   path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
   command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-setting.php",
