1ce743ab405ab705bd32720821223f5456fd766c
KatyaKalache <katya@kalache.fr>
2017-09-09 05:53:21 +0000

    typo fix

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # editing class-wp-locale.phpp name to .php
 exec { '/var/www/html/wp-setting.php':
   path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
-  command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-setting.php",
+  command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+}
