b5151313bf753091550939925abee7147c29dd9f
KatyaKalache <katya@kalache.fr>
2017-09-09 05:55:21 +0000

    adding path from env

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # editing class-wp-locale.phpp name to .php
 exec { '/var/www/html/wp-setting.php':
-  path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+  path => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
   command  => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
 }
