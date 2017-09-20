74f8022858631cfc5dffc6f1592f952871f21dbf
KatyaKalache <katya@kalache.fr>
2017-09-20 05:03:36 +0000

    adding nginx restart command

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,8 @@ exec {'/etc/nginx/nginx.conf':
   path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
   command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf",
 }
+
+exec {'restart nginx after editing congfig file':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "service nginx restart"
+}
