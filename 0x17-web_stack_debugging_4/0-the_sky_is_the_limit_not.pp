59727560c1a530f3655142fd03bc7d356c3f7825
KatyaKalache <katya@kalache.fr>
2017-09-20 17:33:43 +0000

    updating ulimit in /etc/default/nginx

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,6 @@
-# increasing the number of worker processses to 7
-exec {'/etc/nginx/nginx.conf':
+# increasing ulimit
+exec {'Changing Ulimit':
   path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
-  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf",
-}
-
-exec {'restart nginx after editing congfig file':
-  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
-  command => "service nginx restart"
+  command => "sed -i 's/15/15000/g' /etc/default/nginx &&
+  	     service nginx restart",
 }
