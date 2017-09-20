d13696462604946febc4d3b64b7d1c15b0360312
KatyaKalache <katya@kalache.fr>
2017-09-20 04:51:07 +0000

    initial

---
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# increasing the number of worker processses to 7
+exec {'/etc/nginx/nginx.conf':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/worker_processes 4;/worker_processes 7;/g' /etc/nginx/nginx.conf",
+}
