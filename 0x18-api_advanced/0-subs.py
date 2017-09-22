9261ac756f11b860d80c249b888308708b20022d
KatyaKalache <katya@kalache.fr>
2017-09-22 03:44:51 +0000

    passing subreddit as a var

--- 0x18-api_advanced/0-subs.py
+++ 0x18-api_advanced/0-subs.py
@@ -6,10 +6,12 @@ import requests
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers"""
     headers = {'User-Agent': 'Mozilla/5.0'}
-    subreddit = 'https://www.reddit.com/r/subreddit/about.json'
-    req = requests.get(subreddit, headers).json()
-    if (req.get('error')):
+    subreddit = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    if subreddit is None:
         return 0
+    req = requests.get(subreddit, headers=headers).json()
     data = req.get('data')
     subscribers = data.get('subscribers')
+    if subscribers is None:
+        return 0
     return subscribers
