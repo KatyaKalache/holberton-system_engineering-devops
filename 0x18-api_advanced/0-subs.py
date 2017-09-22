96c5b889c0150169d329f734670f2314dc608edb
KatyaKalache <katya@kalache.fr>
2017-09-22 02:48:34 +0000

    adding real info for header

--- 0x18-api_advanced/0-subs.py
+++ 0x18-api_advanced/0-subs.py
@@ -6,9 +6,9 @@ import requests
 def number_of_subscribers(subreddit):
     if subreddit is None:
         return 0
-    headers = {'user-agent': 'info'}
+    header = {'User-Agent': 'Mozilla/5.0'}
     subreddit = 'https://www.reddit.com/r/subreddit/about.json'
-    req = requests.get(subreddit, headers).json()
+    req = requests.get(subreddit, headers=header).json()
     if (req.get('error')):
         return 0
     data = req.get('data')
