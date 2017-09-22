3b7358f121cf48fe109ad6af8cffb05e550a97fe
KatyaKalache <katya@kalache.fr>
2017-09-22 02:50:08 +0000

    passing headers directly in the get request

--- 0x18-api_advanced/0-subs.py
+++ 0x18-api_advanced/0-subs.py
@@ -6,9 +6,8 @@ import requests
 def number_of_subscribers(subreddit):
     if subreddit is None:
         return 0
-    header = {'User-Agent': 'Mozilla/5.0'}
     subreddit = 'https://www.reddit.com/r/subreddit/about.json'
-    req = requests.get(subreddit, headers=header).json()
+    req = requests.get(subreddit, headers = {'User-Agent': 'Mozilla/5.0'}).json()
     if (req.get('error')):
         return 0
     data = req.get('data')
