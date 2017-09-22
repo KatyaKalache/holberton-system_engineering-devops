ac17fac213453da06dd1eeb6d3ef390a40cde63b
KatyaKalache <katya@kalache.fr>
2017-09-22 02:15:53 +0000

    error check

--- 0x18-api_advanced/0-subs.py
+++ 0x18-api_advanced/0-subs.py
@@ -5,6 +5,8 @@ import requests
 def number_of_subscribers(subreddit):
     subreddit = 'https://www.reddit.com/r/subreddit/about.json'
     req = requests.get(subreddit).json()
+    if (req.get('error')):
+        return 0
     data = req.get('data')
     subscribers = data.get('subscribers')
     return subscribers
