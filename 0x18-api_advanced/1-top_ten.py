a6ad70bfdaa056b4f6e4af6fc5a4eb6fccd6c62e
KatyaKalache <katya@kalache.fr>
2017-09-22 03:15:40 +0000

    initial

---
+++ 0x18-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """return the number of subscribers"""
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    subreddit = 'https://www.reddit.com/r/subreddit/hot.json'
+    req = requests.get(subreddit, headers).json()
+    if (req.get('error')):
+        return 0
+    data = req.get('data')
+    top_ten = data.get('children')
+
+    for each in top_ten:
+        return (each)
