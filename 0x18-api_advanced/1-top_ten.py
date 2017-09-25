1937b695485dcc4fe1494e71d2bc0c9064069a89
KatyaKalache <katya@kalache.fr>
2017-09-25 06:33:59 +0000

    adding check if loop was never enetered, return none

--- 0x18-api_advanced/1-top_ten.py
+++ 0x18-api_advanced/1-top_ten.py
@@ -7,20 +7,17 @@ def top_ten(subreddit):
     """Returns top ten posts"""
     title_list = []
     counter = 0
-    if subreddit is None:
-        print ("None")
     headers = {'User-Agent': 'Mozilla/5.0'}
     subreddit = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     req = requests.get(subreddit, headers=headers)
-    if (req.status_code == 301):
-        print ("None")
     top_ten = req.json().get('data', {}).get('children')[:10]
-    if top_ten is None:
-        print ("None")
     for each in top_ten:
         title_list.append(each.get('data').get('title'))
         if (counter != 9):
             title_list.append('\n')
         string_format = ''.join(title_list)
         counter += 1
+    if counter == 0:
+        print ('None')
+        return
     print (string_format)
